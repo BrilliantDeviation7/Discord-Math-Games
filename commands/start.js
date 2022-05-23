e3361d01660e58b8ebe21d0a28f136e764b994f8
BDimension7 <54754524+BDimension7@users.noreply.github.com>
2022-05-23 17:35:51 -0400

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,19 +1,29 @@
 const Database = require("@replit/database");
 const db = new Database();
-const tools = require("../tools");
+const { deleteInvalidCommand } = require("../tools");
 
 module.exports.run = async (client, msg, args) => {
-    if (msg.author.id != process.env.ADMIN) return tools.deleteInvalidCommand(msg, "Not authorized!");
-    let start = await db.get("start_time");
-    if (start) return tools.deleteInvalidCommand(msg, "Game already started!");
-    startTime = Date.now();
-    db.set("start_time", startTime);
-    db.set("last_reap", startTime);
-    db.set("game_scores", []);
-    msg.channel.send(`${msg.author.username} started new game!`);
-    msg.delete();
+    if (args.length < 1) {
+        return deleteInvalidCommand(msg, "What game are we playing today?");
+    }
+    const game = args[0].toLowerCase();
+    if (game == "reaper") {
+        if (msg.author.id != process.env.ADMIN) return deleteInvalidCommand(msg, "Not authorized!");
+        let start = await db.get("start_time");
+        if (start) return deleteInvalidCommand(msg, "Game already started!");
+        startTime = Date.now();
+        db.set("start_time", startTime);
+        db.set("last_reap", startTime);
+        db.set("game_scores", []);
+        msg.channel.send(`${msg.author.username} started new game!`);
+        msg.delete();
 
-    let user_scores = await db.get("user_scores");
-    if (!user_scores) db.set("user_scores", []);
+        let user_scores = await db.get("user_scores");
+        if (!user_scores) db.set("user_scores", []);
+    } else if (game == "greed") {
+        return deleteInvalidCommand(msg, "That game is not complete! BDimension7 is currently developing Greed Control.");
+    } else {
+        return deleteInvalidCommand(msg, "That game is not available!");
+    }
 }
 exports.name = "start"
